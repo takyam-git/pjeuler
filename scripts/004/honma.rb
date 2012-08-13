8dc932a44fbcd6edb2819028e99a004d1393baf6
kazuhiro homma <kazu.homma@gmail.com>
2012-08-13 21:16:37 +0900

    これはもう限界。。。

--- scripts/004/honma.rb
+++ scripts/004/honma.rb
@@ -1,20 +1,3 @@
-# 左右どちらから読んでも同じ値になる数を回文数という。 2桁の数の積で表される回文数のうち、最大のものは 9009 = 91 × 99 である。
-# では、3桁の数の積で表される回文数のうち最大のものはいくらになるか。
-
-# 2桁
-# n = (1..9).to_a # => [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# # 2桁の数字を生成
-# ary = n.product([0, *n]).map {|a|a.join.to_i}
-# # 2桁 x 2桁のすべての組み合わせの積を大きい順に並べた中から回文数であるものを選択
-# palindromic_ary = ary.product(ary).map {|a, b| a * b}.select do |n|
-#   num_str = n.to_s
-#   num_str == num_str.reverse
-# end
-# p palindromic_ary.max # => 9009
-
-# 3桁
-# 上のアルゴリズムだと1秒かかって遅いので別のアルゴリズムに変更
-t = Time.now
 n = (1..9).to_a
 palindromic_num = 0
 # 3桁の数の積を大きい順に並べた配列を生成
@@ -34,4 +17,3 @@ ary.each do |n|
   end
 end
 p palindromic_num
-p ((Time.now - t) * 1000).round
