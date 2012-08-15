d4e6437796158457fff0930c47b6c8e6d07c1319
kazuhiro homma <kazu.homma@gmail.com>
2012-08-15 22:28:17 +0900

    honma.rb改良、honma_kai.rbを追加

--- scripts/010/honma.rb
+++ scripts/010/honma.rb
@@ -1,24 +1,19 @@
 # 10以下の素数の和は2 + 3 + 5 + 7 = 17である.
 # 200万以下の全ての素数の和を計算しなさい.
-def prime?(num, ary)
-  ary.each do |n|
-    break if n * n  > num
-    return false if num % n == 0
-  end
-  return true
-end
-n = 0
-prime_ary = [2, 3, 5]
-while  prime_ary[-1] < 2_000_000
-  n += 1
-  prime = 6 * n + 1
-  if prime?(prime, prime_ary)
-    prime_ary.push(prime)
-  end
-  prime += 4
-  if prime?(prime, prime_ary)
-    prime_ary.push(prime)
+t = Time.now
+max_num = 2_000_000
+prime = Array.new(max_num, true)
+prime[0] = false
+(1..Math.sqrt(max_num).to_i).each do |i|
+  if prime[i]
+    ((i + 1)..(max_num / (i + 1).to_f).to_i).each do |j|
+      prime[(i + 1)* j - 1] = false
+    end
   end
 end
-prime_ary.pop
-p prime_ary.inject(:+)
+sum = 0
+(0..(max_num - 1)).each do |i|
+  sum += (i + 1) if prime[i]
+end
+p sum
+p Time.now - t
---
+++ scripts/010/honma_kai.rb
@@ -0,0 +1,22 @@
+t = Time.now
+max = Integer(ARGV.shift || 2_000_000)
+max2 = (max-3)/2
+for_end = Integer((Math.sqrt(max)-3)/2)
+sieve = Array.new(max2, true)
+# たぶん素数が存在する番号をここで計算している
+for i in 0 .. for_end
+  next unless sieve[i]
+  k = i+i+3
+  j = k*(i+1)+i
+  while j <= max2
+    sieve[j] = nil
+    j += k
+  end
+end
+for i in 0 .. max2
+  sieve[i] && sieve[i]=i+i+3
+end
+sieve.compact!
+sieve.unshift(2)
+p sieve.inject(&:+)
+p Time.now - t
