31c49120baa9ee3c87a9ab229cfdb74c65cb2d02
tatsuki yokomine <tatsuki.yokomine@gaiax.com>
2012-08-20 08:43:27 +0900

    tyokomine 004

---
+++ scripts/004/tyokomine.rb
@@ -0,0 +1,34 @@
+#! ruby -Kui
+
+#回分数かどうか判定
+def check(num)
+  num.split('')
+  if num.size != 6
+   return false
+  end
+  if num[0]==num[5] && num[1]==num[4] && num[2]==num[3]
+    return true
+  else
+    return false
+  end
+end
+def kaibun()
+  result = 0
+  flag = 0
+  max = 0
+  p check('891190')
+  999.downto(100){ |n1|
+    999.downto(100){ |n2|
+      result = n1*n2
+      if check(result.to_s)
+         if(max < result)
+           max = result
+         end
+      end
+   }
+  }
+  return max
+end
+
+p kaibun
+
