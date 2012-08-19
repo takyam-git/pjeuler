b1e6daef6b1a49247bcacc74c8c5d711a877c958
tatsuki yokomine <tatsuki.yokomine@gaiax.com>
2012-08-20 08:28:50 +0900

    tyokomine 002

--- scripts/002/tyokomine.rb
+++ scripts/002/tyokomine.rb
@@ -3,17 +3,14 @@
 fib = Array.new
 fib[0]=1.to_i
 fib[1]=2.to_i
-sum = 0
+sum = 2
 
 
 (1..4000000).each do |i|
-  sum=0
   n = i.to_i-1
   fib[n+2]=fib[n].to_i+fib[n+1].to_i
-  fib.each do |f|
-    if(f.to_i%2==0)
-     sum += f.to_i
-    end
+  if(fib[n+2].to_i%2==0)
+    sum += fib[n+2].to_i
   end
   if sum > 4000000
     break
