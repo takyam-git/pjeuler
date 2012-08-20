afe385f12512fd12e135cead0fb1698e4c2f90bd
kazuhiro homma <kazu.homma@gmail.com>
2012-08-20 13:54:32 +0900

    コメントアウト行か何かでエラーっぽかったので削除した

--- scripts/017/honma.rb
+++ scripts/017/honma.rb
@@ -1,11 +1,3 @@
-#!/usr/bin/env ruby
-# encoding : utf-8
-# Problem 17  †
-# 1 から 5 までの数字を英単語で書けば one, two, three, four, five であり、全部で 3 + 3 + 5 + 4 + 4 = 19 の文字が使われている。
-# では 1 から 1000 (one thousand) までの数字をすべて英単語で書けば、全部で何文字になるか。
-# 注: 空白文字やハイフンを数えないこと。例えば、342 (three hundred and forty-two) は 23 文字、115 (one hundred and fifteen) は20文字と数える。なお、"and" を使用するのは英国の慣習。
-
-# 他と組み合わせない系
 first = %w(one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
 # 他と組み合わせる系
 twent = %w(twent thirt fort fift sixt sevent eight ninet )
