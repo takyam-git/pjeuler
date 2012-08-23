37004dfb5d98abaef41d43aaef70fdafebf6d5cd
tatsuki yokomine <tatsuki.yokomine@gaiax.com>
2012-08-24 08:43:24 +0900

    009りふぁくた

--- scripts/009/tyokomine.rb
+++ scripts/009/tyokomine.rb
@@ -1,16 +1,12 @@
 # encoding:utf-8
 
 def calc()
-for i in 1..300
-  for j in i..400
-    a = i
-    b = j
-    c = Math.sqrt(i*i + j*j)
-    if c.to_i == c
-      if a+b+c==1000
-        return a*b*c.to_i
-      end
-    end
+for a in 1..300
+  for b in a..400
+    c = 1000-(a + b)
+    if a*a+b*b == c*c
+     return a*b*c
+   end
   end
 end
 return 0
