186e9f1846bb135f0008fa0e8cc2cc4dc3e00544
tatsuki yokomine <tatsuki.yokomine@gaiax.com>
2012-09-10 12:28:13 +0900

    scripts/020/tyokomine.rb

--- scripts/003/tyokomine.rb
+++ scripts/003/tyokomine.rb
@@ -4,7 +4,7 @@ num = 600851475143
 renum = num
 ans = 0
 2.upto(num){|i|
-  if renum % i == 0 && i.prime?
+  if i.prime? && renum % i == 0
    ans=renum/i
    break if ans.prime? || i*i>num
    renum = ans
