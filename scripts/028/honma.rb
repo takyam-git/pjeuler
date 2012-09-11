4095973bdcfdcd90b59a3613b7a74e5e5ffe6412
kazuhiro homma <kazu.homma@gmail.com>
2012-09-11 10:18:30 -0400

    リファクタリング

--- scripts/028/honma.rb
+++ scripts/028/honma.rb
@@ -2,20 +2,14 @@
 # encoding : utf-8
 # Problem 28  †
 # 1から初めて右方向に進み時計回りに数字を増やしていき, 5×5の螺旋が以下のように生成される:
-# 21222324252078910196121118543121716151413
+# 21 22 23 24 25
+# 20  7  8  9 10
+# 19  6  1  2 11
+# 18  5  4  3 12
+# 17 16 15 14 13
 # 両対角線上の数字の合計は101であることが確かめられる.
 # 1001×1001の螺旋を同じ方法で生成したとき, 対角線上の数字の合計はいくつだろうか?
 num = 1001
-sum = count = num * num
-hikukazu = (num - 1)
-catch(:finish) do
-  while true
-    4.times do
-      count -= hikukazu
-      sum += count
-      throw :finish if count == 1
-    end
-    hikukazu -= 2
-  end
-end
-p sum
+p 3.step(num, 2).inject(1) {|sum, n|
+  sum += 2 * (2 * n * n - 3 * (n - 1))
+}
