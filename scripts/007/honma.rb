f37cd7a9777c22ee3d09e1f6be372fd692b3225e
kazuhiro homma <kazu.homma@gmail.com>
2012-09-16 07:56:04 +0900

    ARGV使っている部分がおかしいかも

--- scripts/007/honma.rb
+++ scripts/007/honma.rb
@@ -1,7 +1,7 @@
 # 素数を小さい方から6つ並べると 2,  3,  5,  7,  11,  13 であり、6番目の素数は 13 である。
 # 10001 番目の素数を求めよ。
-max = Integer(ARGV.shift || 10001)
-def kh_prime?(num, ary)
+max = 10001
+def prime?(num, ary)
   ary.each do |n|
     break if n * n  > num
     return false if num % n == 0
@@ -13,11 +13,11 @@ prime_ary = [2, 3, 5]
 while  prime_ary.count < max
   n += 1
   prime = 6 * n + 1
-  if kh_prime?(prime, prime_ary)
+  if prime?(prime, prime_ary)
     prime_ary.push(prime)
   end
   prime += 4
-  if kh_prime?(prime, prime_ary)
+  if prime?(prime, prime_ary)
     prime_ary.push(prime)
   end
 end
