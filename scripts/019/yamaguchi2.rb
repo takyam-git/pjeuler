5c370f2caf94006e8a62f29d2221d362dbdea053
takyam <takayuki.yamaguch.m@gmail.com>
2012-09-15 12:05:28 +0900

    add yamaguchi2

--- scripts/019/yamaguchi2.rb
+++ scripts/019/yamaguchi2.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 year = 1900
 month = 1
 day = 1
@@ -11,12 +12,13 @@ while true
   case month
     when 2
       if !(year % 4 != 0 or (year % 400 != 0 and year % 100 == 0))
-        day += 1
+        day += 1 #うるう年は 29 / 7 = 4 余り 1 なので+1曜日
+        # うるう年じゃない場合は 28/7=4余り０なので加算せず
       end
     when 4,6,9,11
-      day += 2
+      day += 2 #30/7 = 4 余り２ なので +2 曜日
     else
-      day += 3
+      day += 3 #31/7 = 4 余り 3 なので +3 曜日
   end
 
   if month == 12
