6241a33cba8571c982d814fd1cd1d7ddcd90e4ca
takyam <takayuki.yamaguch.m@gmail.com>
2012-09-15 17:12:26 +0900

    modify yamaguchi 024

--- scripts/024/yamaguchi.rb
+++ scripts/024/yamaguchi.rb
@@ -1,1 +1,5 @@
-p (0..9).to_a.permutation.map{|numbers|numbers.join('').to_i}.sort[999_999]
+limit = 1_000_000
+minus = (1..9).to_a.inject(&:*)
+ignore = limit / minus
+numbers = (0..9).select{|number| number != ignore}
+p (ignore.to_s + numbers.permutation.map{|num|num.join('').to_i}.sort[limit - minus * ignore - 1].to_s).to_i
