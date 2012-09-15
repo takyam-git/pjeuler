89280a28a526ab1174bc828acd63c67cc1fca202
kazuhiro homma <kazu.homma@gmail.com>
2012-09-16 07:45:26 +0900

    新システムだと落ちてたので試しにclassで囲ってみた

--- scripts/007/honma.rb
+++ scripts/007/honma.rb
@@ -1,24 +1,26 @@
 # 素数を小さい方から6つ並べると 2,  3,  5,  7,  11,  13 であり、6番目の素数は 13 である。
 # 10001 番目の素数を求めよ。
-max = Integer(ARGV.shift || 10001)
-def prime?(num, ary)
-  ary.each do |n|
-    break if n * n  > num
-    return false if num % n == 0
+class HOMMA_PROBREM_007
+  max = Integer(ARGV.shift || 10001)
+  def self.prime?(num, ary)
+    ary.each do |n|
+      break if n * n  > num
+      return false if num % n == 0
+    end
+    return true
   end
-  return true
+  n = 0
+  prime_ary = [2, 3, 5]
+  while  prime_ary.count < max
+    n += 1
+    prime = 6 * n + 1
+    if prime?(prime, prime_ary)
+      prime_ary.push(prime)
+    end
+    prime += 4
+    if prime?(prime, prime_ary)
+      prime_ary.push(prime)
+    end
+  end
+  p prime_ary[-1]
 end
-n = 0
-prime_ary = [2, 3, 5]
-while  prime_ary.count < max
-  n += 1
-  prime = 6 * n + 1
-  if prime?(prime, prime_ary)
-    prime_ary.push(prime)
-  end
-  prime += 4
-  if prime?(prime, prime_ary)
-    prime_ary.push(prime)
-  end
-end
-p prime_ary[-1]
