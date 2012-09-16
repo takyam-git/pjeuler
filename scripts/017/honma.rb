5ecfa9fbf4b5be3a9e1df57d22aff6948a9ae7c7
kazuhiro homma <kazu.homma@gmail.com>
2012-09-16 14:32:45 +0900

    20倍早くなった

--- scripts/017/honma.rb
+++ scripts/017/honma.rb
@@ -1,30 +1,30 @@
-first = %w(one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+# 他と組み合わせない系
+first = %w(one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen).map(&:length)
 # 他と組み合わせる系
-twent = %w(twent thirt fort fift sixt sevent eight ninet )
-twent_end = %w(y y)
-one = [""] + %w( one two three four five six seven eight nine)
-hundredth = " hundred"
-hundred = " hundred and "
-number = []
+twent = %w(twenty thirty forty fifty sixty seventy eighty ninety ).map(&:length)
+one = ([""] + %w( one two three four five six seven eight nine)).map(&:length)
+hundredth = "hundred".length
+hundred = "hundredand".length
+
+number = 0
 one.each do |hund|
   # 100, 200, 300, ...
-  if hund != ""
-    number << hund + hundredth
+  if hund != 0
+    number += (hund + hundredth)
     hund = hund + hundred
   end
 
   # 1 ~ 19
   first.each do |fir|
-    number << hund + fir
+    number += (hund + fir)
   end
   # 20 ~ 99
   twent.each do |twen|
-    number << hund + twen + twent_end[0]
+    number += (hund + twen)
     first[0..8].each do |fir|
-      number << hund + twen + twent_end[1] + " " + fir
+      number += (hund + twen + fir)
     end
   end
 end
-number << "one thousand"
-# number.map{|n|p n}
-p number.map{|n|n.gsub(/ /, "")}.map(&:length).inject(&:+)
+number += "onethousand".length
+p number
