86e5f8b89421e32d25c0e1580a7538c34d791e66
kazuhiro homma <kazu.homma@gmail.com>
2012-09-16 10:21:19 +0900

    エラーよくわからないので一旦戻す

--- multiple/benchmark_scripts/benchmark.js
+++ multiple/benchmark_scripts/benchmark.js
@@ -1,21 +1,21 @@
-function fileCheck( filename ){
-  var fs = require("fs");
-  try {
-    fs.statSync(filename);
-  } catch(e) {
-    console.log("999.999\nERROR SCRIPT NOT FOUND");
-    process.exit();
-  }
-};
+// function fileCheck( filename ){
+//   var fs = require("fs");
+//   try {
+//     fs.statSync(filename);
+//   } catch(e) {
+//     console.log("999.999\nERROR SCRIPT NOT FOUND");
+//     process.exit();
+//   }
+// };
 var filename = process.argv[2] || "";
-fileCheck(filename);
+// fileCheck(filename);
 var microtime = require('microtime')
 var stdout = "";
 console.log = function (d) {
   stdout = d;
 };
 var start = microtime.now();
-//var out = eval(code);
+// eval(code);
 require(filename); //js eval is very very slow
 var end = microtime.now();
 console.log = function (d) {
