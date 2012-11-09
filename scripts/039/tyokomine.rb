ef45848673520fbcfe333dcc71a65e1c998bf93e
tatsuki yokomine <z.ibra.veste@gmail.com>
2012-11-09 11:01:13 +0900

    down loop num

--- scripts/039/tyokomine.rb
+++ scripts/039/tyokomine.rb
@@ -4,11 +4,12 @@
 #p ≦ 1000 で解の数が最大になる p を求めよ.
 
 ans = Array.new
-for a in 1..999
-  for b in 1..999
+for a in 1..334
+  for b in a+1..600
      c = Math.hypot(a,b)
      break if a+b+c>1000
      p_num = a+b+c
+     #cが整数のときは直角
      if c == c.to_i
        if ans[p_num] == nil
          ans[p_num] = 1
@@ -20,6 +21,7 @@ for a in 1..999
 end
 max=0
 result = 0
+#配列バージョン
 ans.each_with_index do |e,i|
  if e != nil
   if max < e
@@ -28,4 +30,11 @@ ans.each_with_index do |e,i|
   end
  end
 end
+#ハッシュバージョン
+#ans.each{|key,value|
+#  if max < value
+#    max = value
+#    result = key
+#  end
+#}
 p result
