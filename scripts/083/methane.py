ab649ad839f6480d9208c19983940dc5f7dc5663
INADA Naoki <inada-n@klab.com>
2013-02-01 20:28:38 +0900

    methane.083

---
+++ scripts/083/methane.py
@@ -0,0 +1,35 @@
+
+def main():
+    from heapq import heapify, heappush, heappop
+
+    M = open('matrix.txt').read().split()
+    M = [map(int, m.split(',')) for m in M]
+
+    H = len(M)
+    W = len(M[0])
+
+    scores = [(M[0][0], 0, 0)]
+    R = [[0]*W for _ in xrange(H)]
+
+    def next(xx, yy):
+        if not R[yy][xx]:
+            ss = M[yy][xx]
+            R[yy][xx] = ss + s
+            heappush(scores, (ss, xx, yy))
+
+    while scores:
+        s, x, y = heappop(scores)
+        if x == W-1 and y == H-1:
+            #for r in R:
+            #    print r
+            return s
+        if y > 0:
+            next(x, y-1)
+        if y < H-1:
+            next(x, y+1)
+        if x > 0:
+            next(x-1, y)
+        if x < W-1:
+            next(x+1, y)
+
+print main()
