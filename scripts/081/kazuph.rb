4a73b0c9592edd9661e068f991c270d84f95f7d4
Kazuhiro Homma <kazu.homma@gmail.com>
2013-02-10 20:07:50 +0900

    改良

--- scripts/081/kazuph.rb
+++ scripts/081/kazuph.rb
@@ -4,12 +4,12 @@ require "pp"
 data = File.read("matrix.txt")
 ary = data.each_line.map{|line| line.split(/,/).map(&:to_i)}
 n = ary.size
-masu = Array.new(n).map! {Array.new(n){|m| m = 99999999999}}
-p masu[0][0] = ary[0][0].to_i
+masu = Array.new(n).map! {Array.new(n)}
+masu[0][0] = ary[0][0]
 n.times do |x|
   n.times do |y|
-    masu[x][y + 1] = masu[x][y] + ary[x][y + 1].to_i if y + 1 < n and (masu[x][y] + ary[x][y + 1].to_i < masu[x][y+1])
-    masu[x + 1][y] = masu[x][y] + ary[x + 1][y].to_i if x + 1 < n and (masu[x][y] + ary[x+1][y].to_i < masu[x+1][y])
+    masu[x][y + 1] = masu[x][y] + ary[x][y + 1] if (y + 1 < n) and (masu[x][y+1].nil? || masu[x][y] + ary[x][y + 1] < masu[x][y+1])
+    masu[x + 1][y] = masu[x][y] + ary[x + 1][y] if (x + 1 < n) and (masu[x+1][y].nil? || masu[x][y] + ary[x+1][y] < masu[x+1][y])
   end
 end
 p masu[-1][-1]
