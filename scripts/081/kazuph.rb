6a3f3d53b2fe38356a26cd776a287b63ec65f205
Kazuhiro Homma <kazu.homma@gmail.com>
2013-02-10 19:56:39 +0900

    今更的に解いてみる

--- scripts/081/kazuph.rb
+++ scripts/081/kazuph.rb
@@ -3,27 +3,13 @@
 require "pp"
 data = File.read("matrix.txt")
 ary = data.each_line.map{|line| line.split(/,/).map(&:to_i)}
-x = ary.length - 1
-y = ary.length - 1
-# p ary[x][y]
-sum = ary[x][y]
-(2 * (ary.length - 1)).times do
-  if x == 0
-    y -= 1
-    sum += ary[x][y]
-    next
+n = ary.size
+masu = Array.new(n).map! {Array.new(n){|m| m = 99999999999}}
+p masu[0][0] = ary[0][0].to_i
+n.times do |x|
+  n.times do |y|
+    masu[x][y + 1] = masu[x][y] + ary[x][y + 1].to_i if y + 1 < n and (masu[x][y] + ary[x][y + 1].to_i < masu[x][y+1])
+    masu[x + 1][y] = masu[x][y] + ary[x + 1][y].to_i if x + 1 < n and (masu[x][y] + ary[x+1][y].to_i < masu[x+1][y])
   end
-  if y == 0
-    x -= 1
-    sum += ary[x][y]
-    next
-  end
-  if ary[x-1][y] < ary[x][y-1]
-    x -= 1
-  else
-    y -= 1
-  end
-  sum += ary[x][y]
-  p [ary[x][y], x, y]
 end
-p sum
+p masu[-1][-1]
