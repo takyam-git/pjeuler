5ea3d7e3bf55e15cafce981c00faadedbb67bcbf
Kazuhiro Homma <kazu.homma@gmail.com>
2013-02-10 20:39:32 +0900

    ちょっと改良

--- scripts/081/kazuph.rb
+++ scripts/081/kazuph.rb
@@ -1,15 +1,30 @@
 #!/usr/bin/env ruby
 # coding : utf-8
-require "pp"
+now = Time.now
 data = File.read("matrix.txt")
-ary = data.each_line.map{|line| line.split(/,/).map(&:to_i)}
-n = ary.size
-masu = Array.new(n).map! {Array.new(n)}
-masu[0][0] = ary[0][0]
+matrix = data.each_line.map{|line| line.split(/,/).map(&:to_i)}
+n = matrix.size
+masu = Array.new(n).map! {Array.new(n) {|m| m = 0}}
+masu[0][0] = matrix[0][0]
 n.times do |x|
   n.times do |y|
-    masu[x][y + 1] = masu[x][y] + ary[x][y + 1] if (y + 1 < n) and (masu[x][y+1].nil? || masu[x][y] + ary[x][y + 1] < masu[x][y+1])
-    masu[x + 1][y] = masu[x][y] + ary[x + 1][y] if (x + 1 < n) and (masu[x+1][y].nil? || masu[x][y] + ary[x+1][y] < masu[x+1][y])
+    if y + 1 < n
+      tmp = masu[x][y] + matrix[x][y + 1]
+      if masu[x][y+1] == 0
+        masu[x][y + 1] = tmp
+      elsif tmp < masu[x][y+1]
+        masu[x][y + 1] = tmp
+      end
+    end
+    if x + 1 < n
+      tmp = masu[x][y] + matrix[x + 1][y]
+      if masu[x + 1][y] == 0
+        masu[x + 1][y] = tmp
+      elsif tmp < masu[x + 1][y]
+        masu[x + 1][y] = tmp
+      end
+    end
   end
 end
 p masu[-1][-1]
+p Time.now - now
