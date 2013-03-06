52e1fe1ebe6d7101d3db5d1c57b9c3a765b83842
Hoto <hoto17296@gmail.com>
2013-03-06 15:38:06 +0900

    refactor

--- scripts/040/hoto17296.rb
+++ scripts/040/hoto17296.rb
@@ -1,16 +1,19 @@
-l = 0
+l = 0  # 現在の桁数
+ll = 0 # 現在の桁数の桁数
 mult = 1
+
 (1..1000000).to_a.each do |n|
   n = n.to_s
-  before_l = l.to_s.length
+  ll_tmp = ll
   l += n.length
-  after_l = l.to_s.length
+  ll = l.to_s.length
 
-  if after_l - before_l > 0
-    idx = n.length - (l - (10**before_l)) - 1
+  # 桁数が桁上りしたらなんやかんやする
+  if ll - ll_tmp > 0
+    break if ll_tmp==6
+    i = n.length - (l - (10**ll_tmp)) - 1
     n = n.split(//)
-    break if before_l==6
-    mult *= n[idx].to_i
+    mult *= n[i].to_i
   end
 end
 
