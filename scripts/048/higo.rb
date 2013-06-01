ab55e351b1e6414f1016e090b5e361e5ec98b5b1
hidehigo <hidehigo@gmail.com>
2013-06-01 13:29:45 +0900

    こっちのが早い

--- scripts/048/higo.rb
+++ scripts/048/higo.rb
@@ -1,12 +1,6 @@
 sumup = 0
 for n in 1 .. 1000
-  tmp = 1
-  n.times do 
-    tmp *= n
-    tmp = tmp % 10_000_000_000
-    #p tmp
-  end
-  sumup += tmp
+  sumup += n ** n
 end
 
 p sumup % 10_000_000_000
