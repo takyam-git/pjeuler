bc093a0a8283d79cabea50c8703faa3f9f716cdf
papix <mail@papix.net>
2013-08-22 20:10:16 +0900

    もう少し早いはず...

--- scripts/052/fukumoto.pl
+++ scripts/052/fukumoto.pl
@@ -5,9 +5,9 @@ my $start = 1;
 
 END: while (1) {
     $start *= 10;
-
-    LOOP1: for my $i ($start..$start * 10 / 6) {
-        LOOP2: for my $j (2..6) {
+    my $i = $start;
+    LOOP1: for my $i ($start .. $start * 10 / 6) {
+        LOOP2: for my $j (2, 3, 4, 5, 6) {
             my $mul = $i * $j;
             for my $check (split //, $i) {
                 next LOOP1 unless $mul =~ /$check/;
